3baf319944841edac3207d8cb7bb6e90fcdf2a55
muller <mullerdannhauser1@gmail.com>
2025-06-18 12:17:19 +0200

    Added directory copy feature

--- src/fileManagement.go
+++ src/fileManagement.go
@@ -194,3 +194,83 @@ func copyFile(src, dst string) error {
 	}
 	return os.Chmod(dst, srcInfo.Mode())
 }
+
+func replicateStructureAndCopy(blueprintDir, sourceDir, destinationDir string) error {
+    fmt.Printf("Using bluprint from: %s\n", blueprintDir)
+    fmt.Printf("Sourcing files from: %s\n", sourceDir)
+    fmt.Printf("Outputing to: %s\n", destinationDir)
+    fmt.Println("------------------------------------")
+
+
+    // Step 1 validate blue print and sources
+
+    for _, path := range []string{blueprintDir, sourceDir}{
+        info, err := os.Stat(path)
+
+        if os.IsNotExist(err){
+            return fmt.Errorf("required directory does not exist: %s", path)
+        }
+        if !info.IsDir() {
+            return fmt.Errorf("Path is not a directory: %s", path)
+        }
+    }
+
+    var notFoundFiles []string
+    var copiedCount int
+
+
+    // Step 2 walk the folder structure and walk the file names
+
+    walkErr := filepath.Walk(blueprintDir, func(blueprintFilePath string, info os.FileInfo, err error) error{
+        if err != nil{
+            return err   
+        }
+        if info.IsDir() {
+            return nil
+        }
+        fileName := info.Name()
+        sourceFilePath := filepath.Join(sourceDir, fileName)  
+
+        if _,err := os.Stat(sourceFilePath); os.IsNotExist(err){
+            log.Printf("SKIPPED: File '%s' not found in  source directory '%s'. \n", fileName, sourceDir)
+            notFoundFiles = append(notFoundFiles, fileName)
+            return nil
+        }
+        relativePath, err := filepath.Rel(blueprintDir, blueprintFilePath)
+        if err != nil{ 
+            fmt.Errorf("could not determine relative path for %s: %w", blueprintFilePath, err) 
+        } 
+
+        destinationFilePath:= filepath.Join(destinationDir, relativePath)
+        // Create destination sub directory
+        destinationSubDir := filepath.Dir(destinationFilePath)
+        if err:= os.MkdirAll(destinationSubDir, os.ModePerm); err != nil{
+            return fmt.Errorf("Could not create destination sub directory %s:%w", destinationSubDir, err)
+        }
+        fmt.Printf("COPYING: %s -> %s\n", sourceFilePath, destinationFilePath)  
+
+        if err := copyFile(sourceFilePath, destinationFilePath); err != nil{
+            log.Printf("ERROR: failed to copy %s:%v")
+        }else{
+            copiedCount++
+        }
+        return nil
+    })
+    if walkErr != nil{
+        return fmt.Errorf("error walking blueprint directory: ", walkErr)
+    }
+
+    fmt.Println("\n----------------------")
+	fmt.Println("Operation Completed.")
+	fmt.Printf("Successfully copied %d files.\n", copiedCount)
+
+	if len(notFoundFiles) > 0 {
+		fmt.Println("\nThe following files from the blueprint were NOT found in the source directory:")
+		for _, file := range notFoundFiles {
+			fmt.Printf("- %s\n", file)
+		}
+	}
+
+
+    return nil
+}
